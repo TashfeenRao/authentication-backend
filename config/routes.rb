ebbd50c7ee388927db7daf64530b1df046fd4bc8
tashfeenrao <raotashfeen25@gmail.com>
2020-08-28 18:39:15 +0500

    logged_in bug fixed

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   get 'registraions/create'
   get 'static/home'
-  get :logged_in, to: "sessions"
+  get :logged_in, to: "sessions#logged_in"
   delete :logout, to: "sessions#logout"
 
   resources :sessions, only: [:create]
